57579711c5c07d83e2a2f6396e1eedb53ac9da91
Adrián García Pitarch <adrian.garcia-pitarch@capgemini.com>
2022-07-12 14:16:01 +0200

    Change run parameters

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     init_db_entities(get_db_settings())
-    uvicorn.run("app:api", host="0.0.0.0", port=get_global_settings().port, log_config=get_log_config(), workers=4)
+    uvicorn.run("app:api", host="0.0.0.0", port=get_global_settings().port, log_config=get_log_config())

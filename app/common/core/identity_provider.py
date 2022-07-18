d568f6ee6014d873a6c303645c423866b22a7f36
LuisPi <44235126+lpinon@users.noreply.github.com>
2022-07-18 08:55:13 +0200

    Dynamic Identity Provider Loader

--- app/common/core/identity_provider.py
+++ app/common/core/identity_provider.py
@@ -29,3 +29,39 @@ class IDPType(Enum):
     KEYCLOAK = 0
     FIREBASE = 1
 
+
+def __get_idp(keycloak_settings: KeycloakSettings | None, firebase_settings: FirebaseSettings | None) -> \
+        (IdentityProvider, IDPType):
+    """Factory that creates the instance of the Identity Provider given the app configuration"""
+    print("Loading IDP Configuration")
+    # Check if configuration is defined to use Keycloak IDP
+    if keycloak_settings is not None and keycloak_settings.auth_server is not None \
+            and keycloak_settings.realm is not None:
+        try:
+            # Configure Keycloak Authentication
+            idp_local = KeycloakService(keycloak_settings)
+            print("Using Keycloak as IDP")
+            return idp_local, IDPType.KEYCLOAK
+        except Exception:
+            print("Error configuring Keycloak")
+            pass
+    # Check if configuration is defined to use Firebase IDP
+    if firebase_settings is not None and firebase_settings.credentials_file is not None:
+        try:
+            # Configure Firebase Service
+            idp_local = FirebaseService(settings=firebase_settings)
+            print("Using Firebase as IDP")
+            return idp_local, IDPType.FIREBASE
+        except Exception:
+            print("Error configuring Firebase")
+            pass
+    return None, None
+
+
+idp_configuration = __get_idp(
+    keycloak_settings=get_keycloak_settings(),
+    firebase_settings=get_firebase_settings()
+)
+
+idp: IdentityProvider | FastAPIKeycloak | FirebaseService = idp_configuration[0]
+idp_type: IDPType = idp_configuration[1]

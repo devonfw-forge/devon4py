d073ee78e53ad327be3e09e847321691e9e4cfb3
LuisPi <44235126+lpinon@users.noreply.github.com>
2022-07-18 08:50:40 +0200

    Fix UUID bug & Refactor

--- app/common/base/base_entity.py
+++ app/common/base/base_entity.py
@@ -4,9 +4,18 @@ from sqlmodel import SQLModel, Field
 from datetime import datetime
 
 
+def new_uuid() -> uuid_pkg.UUID:
+    # Note: Work around UUIDs with leading zeros: https://github.com/tiangolo/sqlmodel/issues/25
+    # by making sure uuid str does not start with a leading 0
+    val = uuid_pkg.uuid4()
+    while val.hex[0] == '0':
+        val = uuid_pkg.uuid4()
+    return val
+
+
 class BaseUUIDModel(SQLModel):
     id: uuid_pkg.UUID = Field(
-        default_factory=uuid_pkg.uuid4,
+        default_factory=new_uuid,
         primary_key=True,
         index=True,
         nullable=False,

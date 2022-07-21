5737120ff59357b9c4e8b354751922696ae78620
LuisPi <44235126+lpinon@users.noreply.github.com>
2022-07-21 12:53:22 +0200

    Back to abstract class

--- app/common/core/identity_provider.py
+++ app/common/core/identity_provider.py
@@ -9,8 +9,9 @@ class User(BaseModel):
     roles: List[str]
 
 
-class IdentityProvider(Protocol):
-    def get_current_user(self, required_roles: list[str] | None = None) -> Callable[[Any], User]:
+class IdentityProvider(abc.ABC):
+    @abc.abstractmethod
+    def get_current_user(self, required_roles: list[str] | None = None):
         """Function that checks the current user based on an access token in the HTTP-header. Optionally verifies
         roles are possessed by the user
 
@@ -26,4 +27,4 @@ class IdentityProvider(Protocol):
             JWTClaimsError: If any claim is invalid
             HTTPException: If any role required is not contained within the roles of the users
         """
-        ...
+        raise NotImplementedError

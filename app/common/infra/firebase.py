95afb94e1222d9fe2fc913faa61fbf9c0073bd3b
Adrián García Pitarch <adrian.garcia-pitarch@capgemini.com>
2022-08-02 14:41:51 +0200

    Update firebase.py

--- app/common/infra/firebase.py
+++ app/common/infra/firebase.py
@@ -1,39 +1,40 @@
-import asyncio
-from typing import TypeVar, Generic, Type, Callable
+import json
+from functools import lru_cache
+from typing import TypeVar, Generic, Type, Callable, overload, Any
 
-from google.cloud.firestore_v1 import AsyncClient, AsyncDocumentReference, AsyncCollectionReference
-from google.cloud.firestore_v1.base_document import BaseDocumentReference
-from google.oauth2 import service_account
-from google.oauth2.service_account import Credentials
+from fastapi import Depends
+from google.cloud.firestore import AsyncClient, AsyncDocumentReference, AsyncCollectionReference
 from pydantic import BaseModel
 
-CREDENTIALS_FILE = 'C:\\Users\\adriagar\\Proyectos\\Wayat\\wayat-flutter-python-mvp\\SPIKES\\Firebase\\wayat-poc-credentials.json'
+CREDENTIALS_FILE = 'C:\\Users\\adriagar\\Proyectos\\devon4py\\devon4py\\wayat-poc-credentials.json'
 
 
 class BaseFirebaseModel(BaseModel):
     document_id: str
 
     class Config:
-        #Temporary solution. Need to create validators for references
         arbitrary_types_allowed = True
+        extra = 'allow'
 
 
 ModelType = TypeVar("ModelType", bound=BaseFirebaseModel)
 
 
-class Location(BaseFirebaseModel):
-    # TODO
-    pass
+def _get_async_client():
+    return AsyncClient.from_service_account_info(_get_account_info())
 
 
-class User(BaseFirebaseModel):
-    trusted_users: list[str]
-    location: BaseDocumentReference
-    do_not_disturb: bool
+@lru_cache
+def _get_account_info():
+    with open(CREDENTIALS_FILE) as f:
+        return json.load(f)
 
 
 class BaseFirestoreRepository(Generic[ModelType]):
-    def __init__(self, *, collection_path: str | tuple[str], model: Type[ModelType], client: AsyncClient):
+    def __init__(self, *,
+                 collection_path: str | tuple[str],
+                 model: Type[ModelType],
+                 client: AsyncClient = Depends(_get_async_client)):
         """
         Object with default methods to Create, Read, Update and Delete (CRUD) from a Firestore Collection.
         """
@@ -51,44 +52,35 @@ class BaseFirestoreRepository(Generic[ModelType]):
         snapshot = await self._get_document_reference(document_id).get()
         return self._model(document_id=snapshot.id, **snapshot.to_dict()) if snapshot.exists else None
 
-    def add_callback(self, document_id: str, callback = Callable):
+    def add_callback(self, document_id: str, callback: Callable):
         # TODO: Async client does not implement on_snapshot, we need to use the sync client
         # return self._get_document_reference(document_id).on_snapshot(callback)
         raise NotImplementedError
 
     async def add(self, *, model: ModelType):
+        await self._get_document_reference(model.document_id).create(model.dict(exclude={"document_id"}))
+
+    @overload
+    async def update(self, *, model: ModelType):
         ...
 
-    async def save(self, *, model: ModelType, refresh=False):
+    @overload
+    async def update(self, *, data: dict[str, Any], document_id: str):
         ...
 
+    async def update(self, *,
+                     model: ModelType | None = None,
+                     data: dict[str, Any] | None = None,
+                     document_id: str | None = None):
+        if model is not None:
+            await self._get_document_reference(model.document_id).update(model.dict(exclude={"document_id"}))
+        elif data is not None and document_id is not None:
+            await self._get_document_reference(document_id).update(data)
+        else:
+            raise ValueError("Either model or (document_id, data) must be passed as argument")
 
-class UsersRepo(BaseFirestoreRepository[User]):
-    pass
-
-
-async def print_users(async_client: AsyncClient):
-    users_collection = async_client.collection("users")
-    users = users_collection.list_documents()
-    async for user_reference in users:
-        user_data = User(**(await user_reference.get()).to_dict())
-        print(user_data)
-
-
-async def print_locations(async_client: AsyncClient):
-    locations_collection = async_client.collection("locations")
-    async for location in locations_collection.list_documents():
-        location_data = (await location.get()).to_dict()
-        print(location_data)
-
-
-async def async_main():
-    alice = await users_repo.get("alice")
-    print(alice)
-
-
-if __name__ == '__main__':
-    credentials: Credentials = service_account.Credentials.from_service_account_file(CREDENTIALS_FILE)
-    client = AsyncClient(credentials=credentials)
-    users_repo = UsersRepo(collection_path="users", model=User, client=client)
-    asyncio.run(async_main())
+    async def delete(self, *, model: ModelType | None = None, document_id: str | None = None):
+        id_to_delete = document_id or (model.document_id if model else None)
+        if not id_to_delete:
+            raise ValueError("Either model or document_id must be passed as argument")
+        await self._get_document_reference(document_id).delete()

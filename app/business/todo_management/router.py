92609e82a9379bd08251c715cfc78efa37d4dff1
LuisPi <44235126+lpinon@users.noreply.github.com>
2023-10-30 12:11:09 +0100

    Refactor API side

--- app/business/todo_management/router.py
+++ app/business/todo_management/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
 # Include all routers here
-from app.business.todo_management.controllers import todo
+from app.business.todo_management.controllers import api_todo
 todo_management_router = APIRouter()
-todo_management_router.include_router(todo.router, tags=["Todo"])
+todo_management_router.include_router(api_todo.router, tags=["Todo"])

b67328dbfd0e979caf6af41403ba1ff810e90eff
LuisPi <44235126+lpinon@users.noreply.github.com>
2023-10-30 17:38:48 +0100

    Base UI views

--- app/business/todo_management/controllers/todo_views.py
+++ app/business/todo_management/controllers/todo_views.py
@@ -6,6 +6,8 @@ from fastapi import APIRouter, Depends, Request, Form, Response
 from starlette.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
 
+from app.business.todo_management.services.todo import TodoService
+
 templates = Jinja2Templates(directory="templates")
 router = APIRouter(prefix="/todo")
 
@@ -15,3 +17,10 @@ logger = logging.getLogger(__name__)
 @router.get("/")
 async def home_page(request: Request):
     return templates.TemplateResponse('todo/index.html', {"request": request})
+
+@router.get("/pending")
+async def pending_todos(request: Request, todo_service: TodoService = Depends(TodoService)):
+    logger.info("Retrieving all the pending TODOs")
+    todos = await todo_service.get_pending_todos()
+    return templates.TemplateResponse('todo/partials/todo_list.html',
+                                      {"request": request, "todos": todos})
